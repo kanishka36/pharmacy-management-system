c1d17642674e497a7a048834531f543ee33acd65
kanishka36 <kanishkaj36@gmail.com>
2024-03-22 16:07:57 +0530

    navbar component add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Home />
     </div>
   );
