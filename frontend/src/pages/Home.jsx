c26b95f25997d9cf6361c841242b67bff3f5f038
kanishka36 <kanishka@smartannualreport.com>
2024-03-23 15:07:15 +0530

    main

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="hero-section "></div>
+    </div>
+  );
 };
 
 export default Home;
