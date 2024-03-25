ed8b6cf9118860207b4ee075867cc237c7437250
kanishka36 <kanishka@smartannualreport.com>
2024-03-26 00:44:29 +0530

    commit

--- frontend/src/pages/DisplayStaff.jsx
+++ frontend/src/pages/DisplayStaff.jsx
@@ -8,7 +8,9 @@ const DisplayStaff = () => {
       <div className="flex">
         <Menu />
         <div className="container mx-auto w-full">
-          <p>view staff</p>
+          <h1 className="text-3xl sm:text-6xl font-medium text-indigo-600 my-3 ml-3">
+            Staff Details
+          </h1>
           <Link to="/dashboard/add-staff">add staff</Link>
         </div>
       </div>
