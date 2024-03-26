8356190a7c295beae03bd429ab06131df8a9e0f7
kanishka36 <kanishka@smartannualreport.com>
2024-03-26 20:42:59 +0530

    navbar and footer hide from dashboard pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Home from "./pages/Home";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Prescription from "./pages/Prescription";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Prescription from "./pages/Prescription";
 import LoginRegister from "./pages/LoginRegister";
 import Shop from "./pages/Shop";
 import Dashboard from "./pages/Dashboard";
@@ -11,22 +11,36 @@ import DisplayStaff from "./pages/DisplayStaff";
 import AddStaff from "./pages/AddStaff";
 
 const App = () => {
+  const shouldHideNavFooter = [
+    "/dashboard",
+    "/dashboard/display-staff",
+    "/dashboard/add-staff",
+  ].includes(window.location.pathname);
+
   return (
-    <div>
-      <BrowserRouter>
-        {/* <Navbar /> */}
+    <BrowserRouter>
+      {shouldHideNavFooter ? (
         <Routes>
-          <Route path="/my-account" element={<LoginRegister />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/prescription" element={<Prescription />} />
-          <Route path="/shop" element={<Shop />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/dashboard/display-staff" element={<DisplayStaff />} />
           <Route path="/dashboard/add-staff" element={<AddStaff />} />
         </Routes>
-        <Footer />
-      </BrowserRouter>
-    </div>
+      ) : (
+        <>
+          <Navbar />
+          <Routes>
+            <Route path="/my-account" element={<LoginRegister />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/prescription" element={<Prescription />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/dashboard/display-staff" element={<DisplayStaff />} />
+            <Route path="/dashboard/add-staff" element={<AddStaff />} />
+          </Routes>
+          <Footer />
+        </>
+      )}
+    </BrowserRouter>
   );
 };
 
