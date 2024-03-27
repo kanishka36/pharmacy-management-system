a82c3fb86bbee901d9f3ff9448cb197d7d6be5da
tharindu748 <161035317+tharindu748@users.noreply.github.com>
2024-03-27 18:12:05 +0530

    nav1

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const Button =(props) => {
     return(
-       <button className='bg-indigo-600 text-white font-[poppins] py-2 px-6 rounded md:ml-8 hover:bg-orange-400 duration-500 '>
+       <button className='bg-indigo-600 text-white font-[poppins] py-2 px-6 rounded  gap-5 hover:bg-orange-400 duration-500 '>
         {props.children}
        </button>
     )
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Button from './Button'; 
+import Button from './Button';
 
 const Navbar = () => {
   const links = [ 
@@ -16,29 +16,33 @@ const Navbar = () => {
   };
 
   const closeMenuOnMobile = () => {
-    if (window.innerWidth <= 1150) {
+    if (window.innerWidth <= 150) {
       setOpen(false);
     }
   };
 
   return (
-    <div className='md:flex items-center justify-between bg-white py-4 px-7 md:px-10'>
+    <div className='md:flex items-center justify-between bg-white py-4 px-7 md:px-5'>
       <div onClick={toggleMenu} className='text-3xl cursor-pointer md:hidden'>
         <ion-icon name={open ? 'close' : 'menu'}></ion-icon>
       </div>
 
-      <nav className='justify-between items-center w-[92%]'>
-        <ul className={`flex items-center flex-gap-[4vw] md:pb-0 pb-12 absolute md:static bg-white left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? "top-20 opacity-100" : "top-[-490px]"} md:opacity-100 opacity-0`} onClick={closeMenuOnMobile}>
+      <nav className={`w-full md:w-auto md:pl-0 pl-9 "px-15 py-15" ${open ? 'block' : 'hidden'} md:block`}>
+        <ul className={`flex flex-col md:flex-row gap-10 items-center justify-center  md:pb-0 pb-12 bg-white absolute md:static w-full left-0 transition-all duration-500 ease-in ${open ? "top-20 opacity-100" : "top-[-490px]"} md:opacity-100 opacity-0`} onClick={closeMenuOnMobile}>
           {links.map((link) => (
             <li key={link.name} className='md:ml-8 text-xl md:my-7'>
               <a href={link.link} className='text-blue-800 hover:text-orange-400'>{link.name}</a>
             </li>
           ))}
-          
-          <div className='flex items-right gap-6 md:w-auto pl-9'>
+        
+        <div className="flex flex-col gap-5  md:flex-row items-center justify-center ">
+          <div className='  text-xl md:my-5'>
             <Button>Upload your Prescription</Button>
+          </div>
+          <div className=' text-xl md:my-7'>
             <Button>Sign in</Button>
           </div>
+        </div>
         </ul>
       </nav>
     </div>
@@ -46,3 +50,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
