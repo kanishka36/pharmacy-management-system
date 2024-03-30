03aca806695de11316f687c64311375ed29ff42d
kanishka36 <kanishka@smartannualreport.com>
2024-03-30 20:18:30 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.config.js";
 
 dotenv.config();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 connectDB();
 
 app.listen(3000, () => {
-  console.log(`Server is running on port 3000`);
+  console.log(`Server is running on port ${PORT}`);
 });
