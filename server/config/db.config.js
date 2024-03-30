e88f6121969f5398ed9e8c11ed402d75d4853908
kanishka36 <kanishka@smartannualreport.com>
2024-03-30 20:17:05 +0530

    commit

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(process.env.MONGODB_URL)
+    await mongoose.connect(process.env.MONGODB_URL)
     console.log("MongoDB is connected");
   } catch (error) {
     console.log(error);
