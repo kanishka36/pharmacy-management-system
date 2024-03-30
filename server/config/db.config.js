ade794b45c03f3166fa1bdfaf66243194e7aff4f
kanishka36 <kanishka@smartannualreport.com>
2024-03-30 19:55:08 +0530

    database connect

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,1 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv"
 
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGODB_URL)
+    console.log("MongoDB is connected");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
