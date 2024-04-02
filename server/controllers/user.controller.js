44fbbf8f455d569f1b2e77fe50314a3da2bf2ad8
kanishka36 <kanishka@smartannualreport.com>
2024-04-02 21:31:39 +0530

    complete user login api

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -53,10 +53,13 @@ const loginUser = asyncHandler(async (req, res) => {
       return;
     }
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const { password: pass, ...rest } = validUser._doc;
+
     res
       .cookie("access_token", token, { httpOnly: true })
       .status(200)
-      .json({ validUser });
+      .json({ rest });
   } catch (error) {
     console.error("Error during user login:", error);
     res.status(500).json({ error: "Internal server error" });
