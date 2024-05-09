65ed4695abb9bf8122a15d682466b277f579d31f
kanishka-jayaruwan <kanishka@smartannualreport.com>
2024-05-09 10:31:16 +0530

    up

--- server/server.js
+++ server/server.js
@@ -10,7 +10,8 @@ import { prescriptionRouter } from "./routes/prescription.routes.js";
 import { cartRouter } from "./routes/cart.routes.js";
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
+const PORT = 3000;
 
 const app = express();
 connectDB();
