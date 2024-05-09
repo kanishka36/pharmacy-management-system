8d15d770a3f32cc134f5847c096403aa73fd3174
kanishka36 <kanishkaj36@gmail.com>
2024-05-09 12:46:20 +0530

    up

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,7 +1,6 @@
 import asyncHandler from "express-async-handler";
 import bcryptjs from "bcryptjs";
-// import User from "../models/user.model.js";
-const User = require("../models/user.model.js")
+import User from "../models/user.model.js";
 import Customer from "../models/customer.model.js";
 import generateToken from "../utils/generateToken.util.js";
 
