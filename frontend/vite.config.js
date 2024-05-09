6c9c23c9f89d0038d2cb7719c00e3010e93e694e
kanishka36 <kanishkaj36@gmail.com>
2024-05-09 15:58:40 +0530

    up

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/': {
-        target: 'https://pharmacy-management-system-rtmh.onrender.com',
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\//, ''),
-        rewrite: (path) => path,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     '/': {
+  //       target: 'https://pharmacy-management-system-rtmh.onrender.com',
+  //       changeOrigin: true,
+  //       // rewrite: (path) => path.replace(/^\//, ''),
+  //       rewrite: (path) => path,
+  //     },
+  //   },
+  // },
 })
